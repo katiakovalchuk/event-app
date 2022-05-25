4d8b85cd12bf4dbf82f768262acb48916c8a34d0
Natalia1919 <gn.natalia19@gmail.com>
2022-05-25 19:38:00 +0300

    Change structure folder pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import {Route, Routes} from "react-router-dom";
-import Home from "./components/Home";
-import LoginPage from "./pages/manager/login";
-import RecoveryPage from "./pages/manager/passwordRecovery";
-import ProfilePage from "./pages/user/profile";
-import MembersPage from "./pages/manager/membersManagement";
-import {AuthContextProvider} from "./context/authContext";
+import { Route, Routes } from "react-router-dom";
+
+import { AuthContextProvider } from "./context/authContext";
+
+import {
+  Home,
+  LoginPage,
+  MembersPage,
+  ProfilePage,
+  RecoveryPage,
+} from "./pages";
 
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-    return (
-        <AuthContextProvider>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/recovery" element={<RecoveryPage/>}/>
-                <Route path="/profile" element={<ProfilePage/>}/>
-                <Route path="/members" element={<MembersPage/>}/>
-            </Routes>
-        </AuthContextProvider>
-    )
+  return (
+    <AuthContextProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/recovery" element={<RecoveryPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/members" element={<MembersPage />} />
+      </Routes>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import styles from "./Profile.module.scss";
-import { auth, db, storage } from "../../../firebase-config";
+
+import { Container, Row, Col } from "react-bootstrap";
+
+import { storage } from "../firebase-config";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+import styles from "../styles/Profile.module.scss";
 
 const ProfilePage = () => {
   const [file, setFile] = useState("");
@@ -17,27 +20,22 @@ const ProfilePage = () => {
       console.log(name);
       const storageRef = ref(storage, file.name);
       const uploadTask = uploadBytesResumable(storageRef, file);
-
       uploadTask.on(
         "state_changed",
         (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           setPerc(progress);
           switch (snapshot.state) {
             case "paused":
-              console.log("Upload is paused");
               break;
             case "running":
-              console.log("Upload is running");
               break;
             default:
               break;
           }
         },
-        (error) => {
-          console.log(error);
-        },
+        (error) => {},
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
             setData((prev) => ({ ...prev, img: downloadURL }));
@@ -59,12 +57,27 @@ const ProfilePage = () => {
           </Col>
           <Col md={4} className="">
             <div className="card text-center align-items-center shadow rounded">
-              <img src={require("../../../assets/images/profile-3.jpg")} className="card-img-top" alt="profile" />
-              <img className={styles.profileImg} src={file ? URL.createObjectURL(file) : require("../../../assets/images/profile-1.png")} alt="profile" />
+              <img
+                src={require("../assets/images/profile-3.jpg")}
+                className="card-img-top"
+                alt="profile"
+              />
+              <img
+                className={styles.profileImg}
+                src={
+                  file
+                    ? URL.createObjectURL(file)
+                    : require("../assets/images/profile-1.png")
+                }
+                alt="profile"
+              />
               <div className="card-body ">
                 <h5 className="card-title">admin</h5>
                 <p>user@gmail.com</p>
-                <a href="mailto:someone@yoursite.com" className="btn btn-warning">
+                <a
+                  href="mailto:someone@yoursite.com"
+                  className="btn btn-warning"
+                >
                   Send Message
                 </a>
               </div>
@@ -74,13 +87,25 @@ const ProfilePage = () => {
               <div className="d-flex flex-row m-2 p-2">
                 <div className="p-1">
                   <label htmlFor="file">
-                    <img className="icon" src={require("../../../assets/images/folder-upload.png")} alt="upload" height={64} />
+                    <img
+                      className="icon"
+                      src={require("../assets/images/folder-upload.png")}
+                      alt="upload"
+                      height={64}
+                    />
                   </label>
-                  <input type="file" id="file" onChange={(e) => setFile(e.target.files[0])} style={{ display: "none" }} />
+                  <input
+                    type="file"
+                    id="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    style={{ display: "none" }}
+                  />
                 </div>
                 <div className="p-1 pt-0">
                   <p className="mb-1 ps-2">Choose Image</p>
-                  <p className="text-muted mb-1 ms-2">JPG, GIF or PNG. MAX size of 800K</p>
+                  <p className="text-muted mb-1 ms-2">
+                    JPG, GIF or PNG. MAX size of 800K
+                  </p>
                 </div>
               </div>
             </div>
