f9b6b28abcca5f94b81e5c44dd43710643e74ca0
katiakovalchuk <kate.kovalchuk225@gmail.com>
2022-06-03 17:54:33 +0300

    add user slice

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,8 +1,69 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import {addNewUser, deleteUser, getUsers, updateUser} from "../../store/slices/usersSlice";
+import {collection, onSnapshot} from "firebase/firestore";
+import {db} from "../../firebase-config";
 
 
 const List = () => {
-    return(
-        <div>Members list</div>
+    const dispatch = useDispatch();
+    const {data: users, status, error} = useSelector(state => state.usersSlice);
+    console.log(users);
+
+    useEffect(() => {
+        dispatch(getUsers());
+    }, [dispatch])
+
+    // const {data, status, error} = useSelector(state => state.usersSlice);
+    // const [users, setUsers] = useState(data);
+    // console.log(data);
+    // console.log(users);
+    //
+    // useEffect(() => {
+    //     dispatch(getUsers());
+    //     setUsers(data);
+    // }, [dispatch]);
+    //
+    // useEffect(() => {
+    //     onSnapshot(collection(db, "users"), snapshot => {
+    //         const snapshotUsers = [];
+    //         snapshot.docs.forEach(doc => {
+    //             snapshotUsers.push({...doc.data(), id: doc.id});
+    //         })
+    //         setUsers(snapshotUsers);
+    //         })
+    // }, [onSnapshot]);
+
+
+    return (
+        <>
+            <ul>
+                <button onClick={e => {
+                    e.preventDefault();
+                    dispatch(addNewUser({name: 'Oleg'}))
+                }}>Add user
+                </button>
+                <button onClick={e => {
+                    e.preventDefault();
+                    dispatch(updateUser({id: "t9RFtLfo2ZD4kSBZ72Zw", name: "Igor", email: "email", phone: "phone"}));
+                }}>
+                    Update user
+                </button>
+                <button onClick={e => {
+                    e.preventDefault();
+                    dispatch(deleteUser("jPSIJMesT6Z65bYrkDHC"));
+                }}>
+                    Delete user
+                </button>
+                {error && <h2>{error}</h2>}
+                {status}
+                {
+                    users && users.map((user, idx) => (
+                        <li key={user.id}>{idx + 1}. {user.name}</li>
+                    ))
+                }
+            </ul>
+        </>
     )
 }
 
---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,108 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {addDoc, collection, deleteDoc, doc, getDocs, updateDoc} from "firebase/firestore";
+
+import {db} from "../../firebase-config";
+
+const initialState = {
+    status: null,
+    error: null,
+    data: []
+}
+
+export const getUsers = createAsyncThunk(
+    "usersSlice/getUsers",
+    async (_, {rejectWithValue}) => {
+        try {
+            const res = await getDocs(collection(db, "users"));
+            return res.docs.map(doc => ({...doc.data(), id: doc.id}));
+        } catch (err){
+            return rejectWithValue(err.message);
+        }
+    }
+);
+
+export const addNewUser = createAsyncThunk(
+    "usersSlice/addNewUser",
+    async (newUser, {rejectWithValue, dispatch}) => {
+        try {
+            const docRef = await addDoc(collection(db, "users"), newUser);
+            return {...newUser, id: docRef.id};
+        } catch (err) {
+            return rejectWithValue(err.message);
+        }
+    }
+)
+
+export const updateUser = createAsyncThunk(
+    "usersSlice/updateUser",
+    async ({id, ...rest}, {rejectWithValue, dispatch, getState}) => {
+        try {
+            await updateDoc(doc(db, "users", id), {...rest}, {merge: true});
+            return {id, ...rest};
+        } catch (err) {
+            return rejectWithValue(err.message);
+        }
+    }
+)
+
+export const deleteUser = createAsyncThunk(
+    "usersSlice/deleteUser",
+    async (id, {rejectWithValue, dispatch}) => {
+        try {
+            await deleteDoc(doc(db, "users", id));
+            return id;
+        } catch (err) {
+            rejectWithValue(err.message);
+        }
+    }
+);
+
+const setError = (state, action) => {
+    state.status = "rejected";
+    state.error = action.payload;
+}
+
+const removeError = state => {
+    state.status = "pending";
+    state.error = null;
+}
+
+const usersSlice = createSlice({
+    name: "usersSlice",
+    initialState,
+    extraReducers: {
+        [getUsers.fulfilled]: (state, action) => {
+            state.status = 'succeeded'
+            state.data = action.payload
+
+        },
+        [addNewUser.fulfilled]: (state, action) => {
+            state.status = "succeeded";
+            state.data.push(action.payload);
+        },
+        [updateUser.fulfilled]: (state, action) => {
+            state.status = "succeeded";
+            state.data = state.data.map(user => {
+                state.status = "succeeded";
+                if (user.id === action.payload.id) {
+                    return {...user, ...action.payload.rest}
+                }
+                return user;
+            })
+        },
+        [deleteUser.fulfilled]: (state, action) => {
+            state.status = "succeeded";
+            state.data = state.data.filter(user => user.id !== action.payload.id);
+        },
+        [getUsers.pending]: removeError,
+        [addNewUser.pending]: removeError,
+        [updateUser.pending]: removeError,
+        [deleteUser.pending]: removeError,
+        [getUsers.rejected]: setError,
+        [addNewUser.rejected]: setError,
+        [updateUser.rejected]: setError,
+        [deleteUser.rejected]: setError
+    }
+})
+
+export default usersSlice.reducer;
