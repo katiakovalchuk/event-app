84786b4620b08e87e0da570de6090cd83b4e83cf
katiakovalchuk <kate.kovalchuk225@gmail.com>
2022-06-07 12:25:00 +0300

    add private routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import EventTable from "../components/EventTable/EventTable";
+import {useSelector} from "react-redux";
 
 const Home = () => {
+
+    const user = {type: "admin"}
+
   return (
     <div className="home_page">
-      <EventTable />
+        {
+            user.type === "user" && <EventTable />
+        }
+        {
+            user.type === "manager" && <div>Manager home</div>
+        }
+        {
+            user.type === "admin" && <div>Admin home</div>
+        }
     </div>
   );
 };
--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -2,18 +2,31 @@ import {Route, Routes} from "react-router-dom";
 
 import Layout from "../components/Layout";
 import {EventsItem, EventsList, Home, MembersPage, NotFoundPage, ProfilePage} from "../pages";
+import ProtectedRoute from "./ProtectedRoute";
 
 export const AppStack = () => {
+    const user = {role: "user"};
+
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
                 <Route index element={<Home/>}/>
-                <Route path="profile" element={<ProfilePage/>}/>
-                <Route path="members" element={<MembersPage/>}/>
-                <Route path="events" element={<EventsList />} />
-                <Route path="events/:id" element={<EventsItem />} />
-                <Route path="*" element={<NotFoundPage/>}/>
+                <Route element={<ProtectedRoute isAllowed={user.role === "user"}/>}>
+                    <Route path="profile" element={<ProfilePage/>}/>
+                    <Route path="members" element={<MembersPage/>}/>
+                    <Route path="events" element={<EventsList/>}/>
+                    <Route path="events/:id" element={<EventsItem/>}/>
+                </Route>
+                <Route element={<ProtectedRoute isAllowed={user.role === "manager"}/>}>
+                    <Route path="event-page"/>
+                    <Route path="members-management"/>
+                </Route>
+                <Route element={<ProtectedRoute isAllowed={user.role === "admin"}/>}>
+                    <Route path="members-management"/>
+                    <Route path="managers-management"/>
+                </Route>
             </Route>
+            <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
     )
 }
