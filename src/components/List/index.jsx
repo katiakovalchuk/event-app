bb54142e53227bd876e1ab8521e610c03dace2cc
Oleksandr Lutsiuk <oleksandr.lutsiuk@gmail.com>
2022-06-10 16:56:32 +0300

    [TASK] add loader, create managment page

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,27 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../../store/slices/usersSlice";
-
+import TableForm from "./TableForm";
 
 const List = () => {
-    const dispatch = useDispatch();
-    const { users } = useSelector(state => state.usersSlice);
+  const [query, setQuery] = useState("");
+  const keys = ["name", "city", "email"];
+  const search = (data) => {
+    if (data.length) {
+      const result = data.filter((item) =>
+        keys.some((key) => item[key].toLowerCase().includes(query))
+      );
+      console.log(result);
+      return result;
+    }
+  };
 
-    useEffect(() => {
-        dispatch(getUsers());
-    }, [dispatch]);
-
-    return (
-        <>
-            <ul>
-                {
-                    users && users.map((user, idx) => (
-                        <li key={user.id}>{idx + 1}. {user.name}</li>
-                    ))
-                }
-            </ul>
-        </>
-    );
+  const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.usersSlice);
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <TableForm data={search(users)} />
+    </>
+  );
 };
 
 export default List;
