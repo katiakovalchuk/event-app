8aae503cf2db60990f4be0109baa948e5f030cd8
Oleksandr Lutsiuk <oleksandr.lutsiuk@gmail.com>
2022-06-16 15:49:10 +0300

    Merge branch 'dev' of https://github.com/katiakovalchuk/event-app into fix-styling

--- src/components/List/TableForm.js
+++ src/components/List/TableForm.js
@@ -1,23 +1,42 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
+import "./style.css";
 
-const TableForm = ({ data }) => {
+const TableForm = ({ data, sortData }) => {
   return (
     <Table striped bordered hover>
       <tbody>
         <tr>
+          <th>Image</th>
           <th>User name</th>
           <th>City</th>
-          <th>E-mail</th>
+          <th
+            onClick={() => {
+              sortData("email");
+            }}
+          >
+            E-mail
+          </th>
+          <th
+            onClick={() => {
+              sortData("scores");
+            }}
+          >
+            Scores
+          </th>
         </tr>
         {data &&
           data.map((user, idx) => (
             <tr key={user.id}>
+              <td className="imageField">
+                <img src={user.image} className="profileImage"></img>
+              </td>
               <td>
                 {idx + 1}. {user.fullName}
               </td>
               <td>{user?.city ?? ""}</td>
               <td>{user.email}</td>
+              <td>{user.scores}</td>
             </tr>
           ))}
       </tbody>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -5,13 +5,15 @@ import TableForm from "./TableForm";
 
 const List = () => {
   const [query, setQuery] = useState("");
-  const keys = ["fullName", "city", "email"];
+  const [localusers, setLocalusers] = useState([]);
+  const keys = ["fullName", "image", "email", "score"];
   const search = (data) => {
     if (data.length) {
       const result = data.filter((item) =>
-        keys.some((key) => item[key]?.toLowerCase().includes(query.toLowerCase()))
+        keys.some((key) =>
+          item[key]?.toLowerCase().includes(query.toLowerCase())
+        )
       );
-      console.log(result);
       return result;
     }
     return null;
@@ -22,15 +24,30 @@ const List = () => {
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (users && !localusers.length) setLocalusers(users);
+  }, [users]);
+
+  const sortData = (field = "scores") => {
+    const copyData = users.concat();
+    const res = copyData.sort((a, b) => {
+      return a[field] > b[field] ? 1 : -1;
+    });
+    setLocalusers(res);
+  };
+
   return (
     <>
-      <input
-        type="text"
-        placeholder="Search..."
-        className="search"
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <TableForm data={search(users)} />
+      <div className="membersForm">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <TableForm data={search(localusers)} sortData={sortData} />
+      </div>
     </>
   );
 };
