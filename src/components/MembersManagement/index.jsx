6c36e5977857d35ab796ca937d6067d5ba85ece5
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-06-26 11:41:27 +0300

    Show date when editing user

--- src/components/MembersManagement/index.jsx
+++ src/components/MembersManagement/index.jsx
@@ -278,6 +278,7 @@ const Table = ({ showManagers }) => {
                 handleEditFormSubmit,
                 handleEditFormChange,
                 addFormData,
+                editFormData,
               }}
             />
           )}
