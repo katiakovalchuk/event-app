2203d7305956206e2714b7a492ad9b1e0ad72093
katiakovalchuk <kate.kovalchuk225@gmail.com>
2022-06-27 00:36:21 +0300

    fix pagination for event users

--- src/components/events/EventMembers.jsx
+++ src/components/events/EventMembers.jsx
@@ -1,30 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { Container } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {Container} from "react-bootstrap";
 
-import {
-  deleteAllMembersFromEvent,
-  getNewMembers,
-  subtractPointsOfAllFromScore,
-} from "../../store/slices/membersSlice";
-import {
-  getNewEvent,
-  deleteNewMembersList,
-} from "../../store/slices/eventSlice";
-import { search } from "../../helpers/utils";
-import { usePagination } from "../../hooks/usePagination";
+import {deleteAllMembersFromEvent, getNewMembers, subtractPointsOfAllFromScore,} from "../../store/slices/membersSlice";
+import {deleteNewMembersList, getNewEvent,} from "../../store/slices/eventSlice";
+import {search} from "../../helpers/utils";
+import {usePagination} from "../../hooks/usePagination";
 
 import EventMember from "./EventMember";
-import { CustomButton } from "../elements";
-import Pagination from "../elements/Pagination";
+import {CustomButton} from "../elements";
+import Pagination from "../Pagination";
 
 const EventMembers = () => {
   const dispatch = useDispatch();
-  const { id } = useParams();
+  const {id} = useParams();
   const currentEvent = useSelector((state) => state.eventSlice.event);
-  const { membersList } = currentEvent;
-  const { members, status } = useSelector((state) => state.membersSlice);
+  const {membersList} = currentEvent;
+  const {members, status} = useSelector((state) => state.membersSlice);
   const [query, setQuery] = useState("");
 
   const eventMembers = members.filter((member) =>
@@ -33,8 +26,8 @@ const EventMembers = () => {
   const keys = ["fullName"];
   const searchedEventMembers = search(eventMembers, keys, query);
 
-  const { pageCount, currentPage, handlePageClick, currentItems } =
-    usePagination({ query, status, data: searchedEventMembers });
+  const {pageCount, currentPage, handlePageClick, currentItems} =
+    usePagination({query, status, data: searchedEventMembers, itemsPerPage: 4});
 
   useEffect(() => {
     dispatch(getNewEvent(id));
@@ -45,7 +38,7 @@ const EventMembers = () => {
   }, [dispatch]);
 
   const deleteAllMembers = (id, points, membersList) => {
-    dispatch(subtractPointsOfAllFromScore({ id, points, membersList }));
+    dispatch(subtractPointsOfAllFromScore({id, points, membersList}));
     dispatch(deleteNewMembersList(id));
     dispatch(deleteAllMembersFromEvent(id));
   };
