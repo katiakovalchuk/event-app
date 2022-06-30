4a9e42baef7b92a5740ea855db781b24d3426c2b
katiakovalchuk <kate.kovalchuk225@gmail.com>
2022-07-01 00:17:27 +0300

    fix show password

--- src/components/forms/PasswordForm.jsx
+++ src/components/forms/PasswordForm.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { EmailAuthProvider } from "firebase/auth";
-import { RiLockPasswordFill } from "react-icons/ri";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import { yupResolver } from "@hookform/resolvers/yup";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
+import {EmailAuthProvider} from "firebase/auth";
+import {RiLockPasswordFill} from "react-icons/ri";
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai";
+import {yupResolver} from "@hookform/resolvers/yup";
 import PropTypes from "prop-types";
 
-import { CustomButton, CustomInput } from "../elements";
-import { useUserAuth } from "../../context/authContext";
-import { useDialog } from "../../context/dialogContext";
-import { errMessages } from "../Login/messages";
-import { passwordSchema } from "../../helpers/schemaForms";
-import { getErrorMessage } from "../../helpers/getErrorMessage";
+import {CustomButton, CustomInput} from "../elements";
+import {useUserAuth} from "../../context/authContext";
+import {useDialog} from "../../context/dialogContext";
+import {errMessages} from "../Login/messages";
+import {passwordSchema} from "../../helpers/schemaForms";
+import {getErrorMessage} from "../../helpers/getErrorMessage";
 
 import "../../styles/form.scss";
 
@@ -20,17 +20,27 @@ const PasswordForm = ({requestData}) => {
     register,
     handleSubmit,
     reset,
-    formState: { errors, isValid },
+    formState: {errors, isValid},
   } = useForm({
     mode: "onChange",
     resolver: yupResolver(passwordSchema),
   });
-  const { user, reauthenticate } = useUserAuth();
-  const { notifyError, addRequireConfirm } = useDialog();
-  const [showPassword, setShowPassword] = useState(false);
+  const {user, reauthenticate} = useUserAuth();
+  const {notifyError, addRequireConfirm} = useDialog();
+  const [{showOldPassword, showNewPassword, showConfirmPassword}, setShowPassword] = useState({
+    showOldPassword: false,
+    showNewPassword: false,
+    showConfirmPassword: false
+  });
 
-  const onShowPassword = () => {
-    setShowPassword((prev) => !prev);
+  const onShowOldPassword = () => {
+    setShowPassword((prev) => ({...prev, showOldPassword: !prev.showOldPassword}));
+  };
+  const onShowNewPassword = () => {
+    setShowPassword((prev) => ({...prev, showNewPassword: !prev.showNewPassword}));
+  };
+  const onShowConfirmPassword = () => {
+    setShowPassword((prev) => ({...prev, showConfirmPassword: !prev.showConfirmPassword}));
   };
 
   const onSubmitPassword = async data => {
@@ -44,7 +54,7 @@ const PasswordForm = ({requestData}) => {
         requestData(data);
         reset();
         addRequireConfirm();
-      } catch (err){
+      } catch (err) {
         notifyError(getErrorMessage(err.message, errMessages));
       }
     } else {
@@ -56,61 +66,61 @@ const PasswordForm = ({requestData}) => {
       <div className="position-relative">
         <CustomInput
           label="Old password"
-          icon={<RiLockPasswordFill />}
-          type={showPassword ? "text" : "password"}
+          icon={<RiLockPasswordFill/>}
+          type={showOldPassword ? "text" : "password"}
           name="oldPassword"
           placeholder="Old password"
           register={register("oldPassword")}
           error={!!errors?.oldPassword}
           errorText={errors?.oldPassword?.message}
           show={
-            showPassword ? (
-              <AiOutlineEyeInvisible size="24px" />
+            showOldPassword ? (
+              <AiOutlineEyeInvisible size="24px"/>
             ) : (
-              <AiOutlineEye size="24px" />
+              <AiOutlineEye size="24px"/>
             )
           }
-          onShowPassword={onShowPassword}
+          onShowPassword={onShowOldPassword}
         />
       </div>
       <div className="position-relative">
         <CustomInput
           label="New password"
-          icon={<RiLockPasswordFill />}
-          type={showPassword ? "text" : "password"}
+          icon={<RiLockPasswordFill/>}
+          type={showNewPassword ? "text" : "password"}
           name="newPassword"
           placeholder="New password"
           register={register("newPassword")}
           error={!!errors?.newPassword}
           errorText={errors?.newPassword?.message}
           show={
-            showPassword ? (
-              <AiOutlineEyeInvisible size="24px" />
+            showNewPassword ? (
+              <AiOutlineEyeInvisible size="24px"/>
             ) : (
-              <AiOutlineEye size="24px" />
+              <AiOutlineEye size="24px"/>
             )
           }
-          onShowPassword={onShowPassword}
+          onShowPassword={onShowNewPassword}
         />
       </div>
       <div className="position-relative">
         <CustomInput
           label="Confirm new password"
-          icon={<RiLockPasswordFill />}
-          type={showPassword ? "text" : "password"}
+          icon={<RiLockPasswordFill/>}
+          type={showConfirmPassword ? "text" : "password"}
           name="confirmNewPassword"
           placeholder="Confirm new password"
           register={register("confirmNewPassword")}
           error={!!errors?.confirmNewPassword}
           errorText={errors?.confirmNewPassword?.message}
           show={
-            showPassword ? (
-              <AiOutlineEyeInvisible size="24px" />
+            showConfirmPassword ? (
+              <AiOutlineEyeInvisible size="24px"/>
             ) : (
-              <AiOutlineEye size="24px" />
+              <AiOutlineEye size="24px"/>
             )
           }
-          onShowPassword={onShowPassword}
+          onShowPassword={onShowConfirmPassword}
         />
       </div>
       <CustomButton className="mt-4" type="submit" disabled={!isValid}>
